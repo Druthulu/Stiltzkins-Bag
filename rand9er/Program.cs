0843c8d6fc511433bbd3389a4ed1f92df37cafc4
Drew <50529377+Druthulu@users.noreply.github.com>
2021-08-06 20:36:59 -0600

    .

---
+++ rand9er/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rand9er
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
